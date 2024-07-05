470bea7b7b9372394a10e74ccf7a5979ec4cc598
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-07-04 18:26:51 -0600

    prepearing deploy

---
+++ src/test/java/com/integrador/eurekasv/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
